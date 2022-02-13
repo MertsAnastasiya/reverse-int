536a7ea1c45a13c47f6caaaadd2d536baff8691b
Anastasiya <Anastasiya@sergeys-mbp.kpn>
2022-02-13 22:05:13 +0100

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let arrayInt = Array.from(String(n));
+    if(arrayInt[0] === '-') {
+        arrayInt.shift();
+    }
+    return Number(arrayInt.reverse().join(''));
 }
